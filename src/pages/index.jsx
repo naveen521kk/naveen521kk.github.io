7d6d13c0c94d6ef01b1b4f3c156e078a17a1ca8c
Naveen M K <naveen521kk@gmail.com>
2021-06-25 15:45:15 +0530

    Initial Move to Gatsby

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import "../styles/index.css";
+
+const IndexPage = () => {
+  return (
+    <main className="maindiv">
+        <div className="aboutmediv">
+            <div className="titlediv">
+                <h1>Hi, I'm Naveen</h1>
+                <img src="images/syrusdark.png" alt="syrusdark"/> 
+                <h2>You can find me in lot of places on the Internet.</h2>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default IndexPage
