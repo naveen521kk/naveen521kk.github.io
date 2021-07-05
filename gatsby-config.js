d7a2a1fe07c7c9d225d63ddcec96854f77f31b2d
Naveen M K <naveen521kk@gmail.com>
2021-07-05 18:40:53 +0530

    Make header better

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-sass`,
         `gatsby-plugin-postcss`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
