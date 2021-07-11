7ecb88cd647354dab4cedd78864c9e1c02d0287b
Naveen M K <naveen521kk@gmail.com>
2021-07-11 21:05:25 +0530

    Use twemoji for handling emoji

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -4,12 +4,19 @@ import NavBar from "../components/header.jsx";
 import SEO from "../components/seo.jsx";
 import BgPhoto from "../components/bgphoto.jsx";
 import "../styles/posts.scss";
+import twemoji from "twemoji";
 
 export default function Template({
     data // this prop will be injected by the GraphQL query below.
 }) {
     const {markdownRemark} = data; // data.markdownRemark holds your post data
     const {frontmatter, html} = markdownRemark;
+    let html_emoji = twemoji.parse(html, {
+        ext: '.svg',
+        callback: function (iconId, options) {
+            return "https://cdn.jsdelivr.net/gh/twitter/twemoji@gh-pages/v/13.1.0/svg/" + iconId + options.ext;
+        }
+    });
     return (
         <article>
             <SEO
@@ -17,14 +24,18 @@ export default function Template({
                 description={frontmatter.description}
             />
             <NavBar />
-            {frontmatter.image ? <BgPhoto img_loc={frontmatter.image} /> : <div />}
+            {frontmatter.image ? (
+                <BgPhoto img_loc={frontmatter.image} />
+            ) : (
+                <div />
+            )}
             <div className="relative mx-auto -m-80 bg-white max-w-3xl rounded-3xl">
-                <div className="blog-post p-9">
+                <div className="blog-post md:p-9 sm:p-3">
                     <h1 className="text-3xl py-2">{frontmatter.title}</h1>
                     <small>{frontmatter.date}</small>
                     <div
                         className="flex flex-col blog-post-content pt-3 mx-auto justify-center"
-                        dangerouslySetInnerHTML={{__html: html}}
+                        dangerouslySetInnerHTML={{__html: html_emoji}}
                     />
                 </div>
             </div>
