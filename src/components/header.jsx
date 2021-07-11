3a5e5abde14ba95c65ebd13d38f9ffe06ae57d76
Naveen M K <naveen521kk@gmail.com>
2021-07-11 21:13:54 +0530

    Make SunIcon default until dark mode someday

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,20 +2,20 @@ import * as React from "react";
 import {Link} from "gatsby";
 import Headroom from "react-headroom";
 import "../styles/header.scss";
-import {MoonIcon} from "./icons";
+import {SunIcon} from "./icons";
 const NavBar = () => {
     return (
         <Headroom>
             <header className="block bg-gradient-to-r from-red-400 via-yellow-500 to-pink-500">
                 <nav className="text-xl flex flex-wrap justify-between leading-12">
                     <div className="flex m-2 items-center">
-                        <img src="/logo.svg" width="40" height="40" className="mr-2"/>
+                        <img src="/logo.svg" width="40" height="40" className="mr-2" alt="Naveen M K"/>
                         <Link to="/" title="Naveen M K (Alt+H)">
                             Naveen M K
                         </Link>
                         <span className="inline-flex mx-5">
-                            <button id="theme-toggle">
-                                <MoonIcon />
+                            <button id="theme-toggle" aria-label="toggle-theme">
+                                <SunIcon />
                             </button>
                         </span>
                     </div>
