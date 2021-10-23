62c78160c4986f7db86052fbd4bd69c7fea3b18f
Naveen M K <naveen521kk@gmail.com>
2021-10-23 20:08:01 +0530

    bgphoto: move to css module

--- src/components/bgphoto.jsx
+++ src/components/bgphoto.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import "../styles/bgphoto.scss";
+import * as bgphotocss from "../styles/bgphoto.module.css";
 
 export default function BgPhoto({img_loc /* image location to be passed*/}) {
-    var style = {"background-image": `url(${img_loc})` };
+    var style = {"background-image": `url(${img_loc})`};
     return (
-        <div className="bg-photo-container">
-            <div className="bg-photo" style={style}></div>
+        <div
+            className={
+                bgphotocss.base_container + " " + bgphotocss.top_container
+            }
+        >
+            <div
+                className={
+                    bgphotocss.base_container + " " + bgphotocss.inner_container
+                }
+                style={style}
+            ></div>
         </div>
     );
 }
