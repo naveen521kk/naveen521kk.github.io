4c4827eb89afbac3b7364992688b1329ad63a55c
Naveen M K <naveen521kk@gmail.com>
2021-10-23 20:16:05 +0530

    index: convert to module css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,23 @@
 import * as React from "react";
-import "../styles/index.css";
+import * as index_css from "../styles/index.module.css";
 import NavBar from "../components/header.jsx";
 import SEO from "../components/seo.jsx";
-import { StaticImage } from "gatsby-plugin-image"
+import {StaticImage} from "gatsby-plugin-image";
 
 const IndexPage = () => {
     return (
         <main>
             <SEO title="Naveen M K (about_me)" />
             <NavBar />
-            <div className="container mx-auto px-5">
-            <h2 className="text-3xl font-bold text-center">This is a WIP page.</h2>
-            <StaticImage src="../assets/images/banner.png" alt="Naveen M K (naveen521kk)" className="block max-w-auto"/>
+            <div className={index_css.container}>
+                <h2 className={index_css.temp_h2}>
+                    This is a WIP page.
+                </h2>
+                <StaticImage
+                    src="../assets/images/banner.png"
+                    alt="Naveen M K (naveen521kk)"
+                    className={index_css.temp_img}
+                />
             </div>
         </main>
     );
