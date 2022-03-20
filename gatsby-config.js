23db0eb157405ca88aaa37350d37b695c06b033d
Naveen M K <naveen521kk@gmail.com>
2022-03-20 12:53:41 +0530

    Try not using some plugins
    makes the site broken

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,37 +50,37 @@ module.exports = {
         `gatsby-transformer-json`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sitemap`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Naveen's Site`,
-                short_name: `naveen`,
-                description: `Random things`,
-                start_url: `/`,
-                background_color: `#fff`,
-                theme_color: `#6B46C1`,
-                display: `standalone`,
-                icons: [
-                    {
-                        src: `/android-chrome-192x192.png`,
-                        sizes: `192x192`,
-                        type: `image/png`
-                    },
-                    {
-                        src: `/android-chrome-512x512.png`,
-                        sizes: `512x512`,
-                        type: `image/png`
-                    }
-                ]
-            }
-        },
+//         {
+//             resolve: `gatsby-plugin-manifest`,
+//             options: {
+//                 name: `Naveen's Site`,
+//                 short_name: `naveen`,
+//                 description: `Random things`,
+//                 start_url: `/`,
+//                 background_color: `#fff`,
+//                 theme_color: `#6B46C1`,
+//                 display: `standalone`,
+//                 icons: [
+//                     {
+//                         src: `/android-chrome-192x192.png`,
+//                         sizes: `192x192`,
+//                         type: `image/png`
+//                     },
+//                     {
+//                         src: `/android-chrome-512x512.png`,
+//                         sizes: `512x512`,
+//                         type: `image/png`
+//                     }
+//                 ]
+//             }
+//         },
         {
             resolve: `gatsby-plugin-google-gtag`,
             options: {
                 trackingIds: ["G-695MYTZSVL"]
             }
         },
-        `gatsby-plugin-offline`,
+//         `gatsby-plugin-offline`,
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
