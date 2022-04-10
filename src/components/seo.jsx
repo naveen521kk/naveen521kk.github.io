17172a0546a230264c60fda04da4b4299fddf3d6
naveen521kk <naveen521kk@gmail.com>
2022-04-10 06:44:21 +0000

    Add canonical url

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Helmet} from "react-helmet";
-import {useLocation} from "@reach/router";
-import {useStaticQuery, graphql} from "gatsby";
+import { Helmet } from "react-helmet";
+import { useLocation } from "@reach/router";
+import { useStaticQuery, graphql } from "gatsby";
 
-const SEO = ({title, description, image, article}) => {
-    const {pathname} = useLocation();
-    const {site} = useStaticQuery(query);
+const SEO = ({ title, description, image, article, canonical }) => {
+    const { pathname } = useLocation();
+    const { site } = useStaticQuery(query);
 
     const {
         defaultTitle,
@@ -20,15 +20,16 @@ const SEO = ({title, description, image, article}) => {
         title: title || defaultTitle,
         description: description || defaultDescription,
         image: `${siteUrl}${image || defaultImage}`,
-        url: `${siteUrl}${pathname}`
+        url: `${siteUrl}${pathname}`,
+        canonical: canonical || `${siteUrl}${pathname}`,
     };
-
     return (
         <Helmet title={seo.title} titleTemplate={titleTemplate}>
             <meta name="description" content={seo.description} />
             <meta name="image" content={seo.image} />
 
             {seo.url && <meta property="og:url" content={seo.url} />}
+            {seo.url && <link rel="canonical" href={seo.canonical} />}
 
             {(article ? true : null) && (
                 <meta property="og:type" content="article" />
@@ -61,14 +62,16 @@ SEO.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
     image: PropTypes.string,
-    article: PropTypes.bool
+    article: PropTypes.bool,
+    canonical: PropTypes.string,
 };
 
 SEO.defaultProps = {
     title: null,
     description: null,
     image: null,
-    article: false
+    article: false,
+    canonical: null,
 };
 
 const query = graphql`
