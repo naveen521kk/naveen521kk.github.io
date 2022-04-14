df7f996ae2fd7673ca8dccf436368f833877164a
naveen521kk <naveen521kk@gmail.com>
2022-04-14 11:24:30 +0000

    A small change to index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,6 +11,9 @@ const IndexPage = () => {
             <NavBar />
             <div className={index_css.container}>
                 <h2 className={index_css.temp_h2}>This is a WIP page.</h2>
+                <h3>Hi I'm Naveen, you can find me on 
+                    <a href="https://github.com/naveen521kk"> Github</a>
+                </h3>
                 <StaticImage
                     src="../assets/images/banner.png"
                     alt="Naveen M K (naveen521kk)"
