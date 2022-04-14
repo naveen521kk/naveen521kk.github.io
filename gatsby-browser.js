261dbea0aa6322c144aabbc07e2ecaa76c62d9e6
naveen521kk <naveen521kk@gmail.com>
2022-04-14 10:58:34 +0000

    Create a global.scss file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,4 @@ import "./src/styles/global.css";
 import "prismjs/themes/prism-solarizedlight.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/command-line/prism-command-line.css";
+import './src/styles/global.scss'
