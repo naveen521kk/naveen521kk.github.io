6151ae0042bc3e7bdf042109cce6c446f54abf3e
naveen521kk <naveen521kk@gmail.com>
2022-04-14 12:33:01 +0000

    small fixup

--- src/components/post-link.jsx
+++ src/components/post-link.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "gatsby";
-import * as PostsList from "../styles/posts-list.module.css";
+import * as PostsList from "../styles/posts-list.module.scss";
 
 const PostLink = ({post}) => (
     <div className={PostsList.image_container}>
