e6a4d399d81bd60393460f247fa83430479a3581
Naveen M K <naveen521kk@gmail.com>
2022-07-22 15:48:07 +0000

    Add a dark and a light logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,20 +3,33 @@ import {Link} from "gatsby";
 import Headroom from "react-headroom";
 import * as header from "../styles/header.module.scss";
 import {DarkModeToggler} from "./ThemeToggler.jsx";
+import {ThemeContext} from "./ThemeContext.js";
 
 const NavBar = () => {
+    const {colorMode} = React.useContext(ThemeContext);
     return (
         <Headroom>
             <header className={header.header}>
                 <nav className={header.main_nav}>
                     <div className={header.nav_inner_div}>
-                        <img
-                            src="/logo.svg"
-                            width="40"
-                            height="40"
-                            className={header.nav_logo}
-                            alt="Naveen M K"
-                        />
+                        {colorMode === "dark" ? (
+                            <img
+                                src="/logo-dark.svg"
+                                width="40"
+                                height="40"
+                                className={header.nav_logo}
+                                alt="Naveen M K"
+                            />
+                        ) : (
+                            <img
+                                src="/logo.svg"
+                                width="40"
+                                height="40"
+                                className={header.nav_logo}
+                                alt="Naveen M K"
+                            />
+                        )}
+
                         <Link to="/" title="Naveen M K (Alt+H)">
                             Naveen M K
                         </Link>
