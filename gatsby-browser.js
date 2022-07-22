69a679735136a8b6ec7f24311ac056a23e5fde1b
Naveen M K <naveen521kk@gmail.com>
2022-07-22 16:58:00 +0000

    Change background color of code based on theme
    Don't use the css from prism directly instead embed it with changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 import "@fontsource/plus-jakarta-sans";
 import "./src/styles/global.css";
-import "prismjs/themes/prism-solarizedlight.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/command-line/prism-command-line.css";
+// import "prismjs/themes/prism-solarizedlight.css";
+// import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+// import "prismjs/plugins/command-line/prism-command-line.css";
 import "./src/styles/global.scss";
 import React from "react";
 
