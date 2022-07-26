264141f1dba54a3a945b0def04983b294a64448a
Naveen M K <naveen521kk@gmail.com>
2022-07-26 07:44:04 +0000

    Display extra navbar at bottom on mobile

---
+++ src/components/WindowSizeContext.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const WindowSizeContext = React.createContext({
+    windowSize: null,
+    setWindowSize: function () {}
+});
+
+const debounce = (func, delay) => {
+    let debounceTimer;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+export const WindowSizeProvider = ({children}) => {
+    const [windowSize, setWindowSize] = React.useState(undefined);
+
+    React.useEffect(() => {
+        setWindowSize(window.innerWidth);
+    }, []);
+    const contextValue = {
+        windowSize,
+        setWindowSize
+    };
+
+    React.useEffect(() => {
+        const handleResize = debounce(function () {
+            setWindowSize(window.innerWidth);
+        }, 500);
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <WindowSizeContext.Provider value={contextValue}>
+            {children}
+        </WindowSizeContext.Provider>
+    );
+};
