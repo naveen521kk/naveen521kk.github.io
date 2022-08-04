80f28437d2f91c3eb2fa3170cebf92fdafcb84d7
Naveen M K <naveen521kk@gmail.com>
2022-08-04 07:45:53 +0000

    Change `class` to `className`
    again, another warning fix

--- src/components/note-block.jsx
+++ src/components/note-block.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import * as noteblockcss from "../styles/note-block.module.scss";
 
 const NoteBlock = ({children}) => (
-    <div class={noteblockcss.admonition}>
-        <p class={noteblockcss.admonition_title}>Note</p>
+    <div className={noteblockcss.admonition}>
+        <p className={noteblockcss.admonition_title}>Note</p>
         <div className={noteblockcss.inner_div}>{children}</div>
     </div>
 );
