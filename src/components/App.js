7268d7efcb8fa81ad445ff870b37ee1469f9a741
Naveen M K <naveen521kk@gmail.com>
2023-05-22 09:07:14 +0530

    Add use reduced motion preference

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,22 @@ import React from "react";
 
 import {ThemeProvider} from "./ThemeContext";
 import {WindowSizeProvider} from "./WindowSizeContext";
+import {useReducedMotion, Globals} from "@react-spring/web";
 
 function App({children}) {
+    const reducedMotion = useReducedMotion();
+    React.useEffect(() => {
+        Globals.assign({
+            skipAnimation: reducedMotion
+        });
+
+        return () => {
+            Globals.assign({
+                skipAnimation: false
+            });
+        };
+    });
+
     return (
         <WindowSizeProvider>
             <ThemeProvider>{children}</ThemeProvider>
