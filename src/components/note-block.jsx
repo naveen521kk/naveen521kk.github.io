a2c15236d70ce74d66cb6b3548aca51c19d7f00c
Naveen M K <naveen521kk@gmail.com>
2024-06-30 23:05:18 +0530

    Add astro to ghost post

--- src/components/note-block.jsx
+++ src/components/note-block.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import * as noteblockcss from "../styles/note-block.module.scss";
 
-const NoteBlock = ({children}) => (
+const NoteBlock = ({children, title = "Note"}) => (
     <div className={noteblockcss.admonition}>
-        <p className={noteblockcss.admonition_title}>Note</p>
+        <p className={noteblockcss.admonition_title}>{title}</p>
         <div className={noteblockcss.inner_div}>{children}</div>
     </div>
 );
